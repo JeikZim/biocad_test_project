b0408a7767c2bf3524f15df8720e1965d13b3ff6
JeikZim <meatcatcher141@gmail.com>
2023-05-02 10:25:41 +0300

    fix header, analytic main top, start building table

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 function noticeSwitch(id) {
     noticeEl = $('#notice' + id)
     if (noticeEl.attr('src').includes('no-notice')) {
-        noticeEl.attr('src', '../assets/notice.svg')
+        noticeEl.attr('src', '../assets/icons/block-notice-icon.svg')
     } 
+    else if (noticeEl.attr('src').includes('block-notice')) {
+        noticeEl.attr('src', '../assets/icons/notice-icon.svg')
+        
+    }
     else {
-        noticeEl.attr('src', '../assets/no-notice.svg')
+        noticeEl.attr('src', '../assets/icons/no-notice-icon.svg')
     }
 
 }
